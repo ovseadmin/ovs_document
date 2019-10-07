d39f44e33fd364b695c5344d707759cbe4edc577
New Mobility TF 안홍범 <hb.ahn@sk.com>
2019-10-07 19:25:12 +0900

    enable sphinx style

--- conf.py
+++ conf.py
@@ -38,8 +38,7 @@ release = u''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -66,7 +65,7 @@ language = None
 exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = sphinx
 
 
 # -- Options for HTML output -------------------------------------------------
